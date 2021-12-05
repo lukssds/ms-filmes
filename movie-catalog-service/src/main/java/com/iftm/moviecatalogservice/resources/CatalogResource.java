1d80ad1a0d79130479faea9e0a8fa9af6b9b81bd
Lucas Samuel <lukssds@gmail.com>
2021-12-05 16:57:22 -0300

    Adicionando Eureka server, Clientes e usando discovery to call Apis

--- movie-catalog-service/src/main/java/com/iftm/moviecatalogservice/resources/CatalogResource.java
+++ movie-catalog-service/src/main/java/com/iftm/moviecatalogservice/resources/CatalogResource.java
@@ -2,14 +2,14 @@ package com.iftm.moviecatalogservice.resources;
 
 import com.iftm.moviecatalogservice.models.CatalogItem;
 import com.iftm.moviecatalogservice.models.Movie;
-import com.iftm.moviecatalogservice.models.Rating;
+import com.iftm.moviecatalogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,18 +21,17 @@ public class CatalogResource {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+        UserRating userRating = restTemplate.getForObject("http://movie-data-service/ratingdata/users/" + userId, UserRating.class);
 
 
-        List<Rating> ratings = Arrays.asList(
-                new Rating("12",15),
-                new Rating("15",20)
-        );
 
-        return ratings.stream().map(rating -> {
-                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+        return userRating.getUserRating().stream().map(rating -> {
+                    Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
                     return new CatalogItem(movie.getName(),"FILMAO",rating.getRating());
                 }).collect(Collectors.toList());
 
--- movie-data-service/src/main/java/com/iftm/moviedataservice/model/UserRating.java
+++ movie-data-service/src/main/java/com/iftm/moviedataservice/model/UserRating.java
@@ -3,13 +3,23 @@ package com.iftm.moviedataservice.model;
 import java.util.List;
 
 public class UserRating {
+    private  String userId;
     private List<Rating> userRating;
 
-    public UserRating(List<Rating> userRating) {
+    public UserRating(String userId, List<Rating> userRating) {
+        this.userId = userId;
         this.userRating = userRating;
     }
 
     public UserRating() {
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
     }
 
     public List<Rating> getUserRating() {
