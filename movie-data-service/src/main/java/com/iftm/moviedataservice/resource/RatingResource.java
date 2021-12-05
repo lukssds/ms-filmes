02d5df482a5c8353e7500edf6e69d6e1999a79ac
Lucas Samuel <lukssds@gmail.com>
2021-12-05 15:51:54 -0300

    Adicionando Discovery-server Eureka

--- movie-data-service/src/main/java/com/iftm/moviedataservice/resource/RatingResource.java
+++ movie-data-service/src/main/java/com/iftm/moviedataservice/resource/RatingResource.java
@@ -1,10 +1,13 @@
 package com.iftm.moviedataservice.resource;
 
 import com.iftm.moviedataservice.model.Rating;
+import com.iftm.moviedataservice.model.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
 
 
 @RestController
@@ -15,4 +18,18 @@ public class RatingResource {
     public Rating getRating(@PathVariable("movieId") String movieId) {
         return new Rating(movieId, 9);
     }
+
+    @RequestMapping("users/{userId}")
+    public List<Rating> getUserRating(@PathVariable("userID") String userId){
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234",18),
+                new Rating("1541",28)
+        );
+        UserRating userRating =  new UserRating();
+        userRating.setUserRating(ratings);
+
+        return ratings;
+    }
+
+
 }
